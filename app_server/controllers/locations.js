f7fb30ac1e372eb71d2b01220c86ff436f89abde
Joseph Bi <xiaozhen.bi@gmail.com>
2016-07-09 14:49:27 +1000

    files for angular

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -72,35 +72,6 @@ module.exports.homelist = function (req, res) {
     };
 }
 
-
-// res.render('locations-list', {
-//     title: 'Loc8r - find a place to work with wifi',
-//     pageHeader: {
-//         title: 'Loc8r',
-//         strapline: 'Find places to work with wifi near you!'
-//     },
-//     locations: [{
-//         name: 'Starcups',
-//         address: '125 High Street, Reading, RG6 1PS',
-//         rating: 3,
-//         facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-//         distance: '100m'
-//     }, {
-//         name: 'Cafe Hero',
-//         address: '125 High Street, Reading, RG6 1PS',
-//         rating: 4,
-//         facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-//         distance: '200m'
-//     }, {
-//         name: 'Burger Queen',
-//         address: '125 High Street, Reading, RG6 1PS',
-//         rating: 2, facilities: ['Food', 'Premium wifi'],
-//         distance: '250m'
-//     }]
-// });
-// }
-// ;
-
 /* GET 'Location info' page */
 module.exports.locationInfo = function (req, res) {
     getLocationInfo(req, res, renderDetailPage);
@@ -156,49 +127,6 @@ var _showError = function(req, res, status) {
         content: content
     });
 }
-
-//
-//     res.render('location-info', {
-//         title: 'Starcups',
-//         pageHeader: {title: 'Starcups'},
-//         sidebar: {
-//             context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
-//             callToAction: 'If you\'ve been and you like it - or if you don\'t - please leave a review to help other people just like you.'
-//         },
-//         location: {
-//             name: 'Starcups',
-//             address: '125 High Street, Reading, RG6 1PS',
-//             rating: 3,
-//             facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-//             coords: {lat: 51.455041, lng: -0.9690884},
-//             openingTimes: [{
-//                 days: 'Monday - Friday',
-//                 opening: '7:00am',
-//                 closing: '7:00pm',
-//                 closed: false
-//             }, {
-//                 days: 'Saturday',
-//                 opening: '8:00am',
-//                 closing: '5:00pm',
-//                 closed: false
-//             }, {
-//                 days: 'Sunday',
-//                 closed: true
-//             }],
-//             reviews: [{
-//                 author: 'Simon Holmes',
-//                 rating: 5,
-//                 timestamp: '16 July 2013',
-//                 reviewText: 'What a great place. I can\'t say enough good things about it.'
-//             }, {
-//                 author: 'Charlie Chaplin',
-//                 rating: 3,
-//                 timestamp: '16 June 2013',
-//                 reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
-//             }]
-//         }
-//     });
-// };
 
 var renderReviewForm = function(req, res, locDetail) {
     res.render('location-review-form', {
